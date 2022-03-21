62f4ab6e0ab097aa0e4965d5e78945ab667d21c9
Fernando Alcaraz <ffernandoalcaraz@gmail.com>
2022-03-21 14:52:21 -0600

    Pruebas de GIT

---
+++ chanchitofeliz.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Chanchito Feliz!";
+    return 0;
+}
---
+++ juanelbosque.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Juan El Bosque.\nPero debo realizar un cambio.";
+    return 0;
+}
